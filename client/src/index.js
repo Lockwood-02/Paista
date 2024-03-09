d67b7a4b3784221876bca679a382330ddd57d19e
Juno <nitrorexxo@gmail.com>
2024-03-09 00:12:06 -0600

    Front End Foundation
    Created the starting point of the front end which includes the sections and the headers with some filler text.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './components/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
 
