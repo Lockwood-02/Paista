a3e07eb83cdb4f8f4fb58d5feb38015691080e79
Andrew Toussaint <ajntoussaint@gmail.com>
2024-03-25 16:41:28 -0500

    implemented client-server communication with axios package. Run client and check localhost:3000/devTest for example of retrieving database data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,21 @@ const port = process.env.PORT || 5000;
 //const dbPath = path.resolve(__dirname, '../data/database.db');
 //const db = new sqlite3.Database(dbPath);
 const {sequelize, Topic} = require('./dataAccessLayer/sequelize.js')//will need to include all table names in the import
+
+//cors setup for communication with front-end
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin); //TODO: This MUST be updated to the production URL
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+}) 
+
+app.use(cors({origin:"http://localhost:3000"})); //can be changed based on env variable
 
 //synchronize to test db setup, developement only
 sequelize.sync().then(() => {
