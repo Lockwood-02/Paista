e7f27e10312000006a08b6a0c49dd70b1a0056b1
Juno <isaaclockwood618@gmail.com>
2024-04-03 11:52:13 -0500

    Stylized New Login and Signup
    Just added some style changes to the new Signup and Login pages and keeps the functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -31,17 +31,34 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required /><br></br>
+        // <div>
+        //     <h1>Login</h1>
+        //     <form onSubmit={handleLogin}>
+        //         <label htmlFor="username">Username:</label>
+        //         <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required /><br></br>
                 
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="hashedPassword" value={password} onChange={(e) => setPassword(e.target.value)} required /><br></br>
-                <button type="submit">Login</button>
+        //         <label htmlFor="password">Password:</label>
+        //         <input type="password" id="password" name="hashedPassword" value={password} onChange={(e) => setPassword(e.target.value)} required /><br></br>
+        //         <button type="submit">Login</button>
+        //     </form>
+        // </div>
+
+    <div className="bg-back flex justify-center items-center h-screen">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">Login</h1>
+            <form onSubmit={handleLogin} className="space-y-4">
+                <div>
+                    <label htmlFor="username" className="block">Username:</label>
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                </div>
+                <div>
+                    <label htmlFor="password" className="block">Password:</label>
+                    <input type="password" id="password" name="hashedPassword" value={password} onChange={(e) => setPassword(e.target.value)} required className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                </div>
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
             </form>
         </div>
+    </div>
     );
 
 };
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -35,26 +35,55 @@ const Signup = () => {
     }
 
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSignup}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required value={username} onChange={(e) => setUsername(e.target.value)} /><br></br>
-                
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} /><br /><br />
+        // <div>
+        // <h1>Sign Up</h1>
+        // <form onSubmit={handleSignup}>
+        //     <label htmlFor="username">Username:</label>
+        //     <input type="text" id="username" name="username" required value={username} onChange={(e) => setUsername(e.target.value)} /><br></br>
+            
+        //     <label htmlFor="password">Password:</label>
+        //     <input type="password" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} /><br /><br />
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
+        //     <label htmlFor="email">Email:</label>
+        //     <input type="email" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
 
-                <label htmlFor="firstName">First Name:</label>
-                <input type="text" id="firstName" name="firstName" required value={firstName} onChange={(e) => setFirstName(e.target.value)} /><br /><br />
+        //     <label htmlFor="firstName">First Name:</label>
+        //     <input type="text" id="firstName" name="firstName" required value={firstName} onChange={(e) => setFirstName(e.target.value)} /><br /><br />
 
-                <label htmlFor="lastName">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" required value={lastName} onChange={(e) => setLastName(e.target.value)} /><br /><br />
+        //     <label htmlFor="lastName">Last Name:</label>
+        //     <input type="text" id="lastName" name="lastName" required value={lastName} onChange={(e) => setLastName(e.target.value)} /><br /><br />
 
-                <button type='submit'>Sign up </button>
-            </form>
+        //     <button type='submit'>Sign up </button>
+        //     </form>
+        // </div>
+
+        <div className="bg-back flex justify-center items-center h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+                <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
+                <form onSubmit={handleSignup} className="space-y-4">
+                    <div>
+                        <label htmlFor="username" className="block">Username:</label>
+                        <input type="text" id="username" name="username" required value={username} onChange={(e) => setUsername(e.target.value)} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block">Password:</label>
+                        <input type="password" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block">Email:</label>
+                        <input type="email" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstName" className="block">First Name:</label>
+                        <input type="text" id="firstName" name="firstName" required value={firstName} onChange={(e) => setFirstName(e.target.value)} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div>
+                        <label htmlFor="lastName" className="block">Last Name:</label>
+                        <input type="text" id="lastName" name="lastName" required value={lastName} onChange={(e) => setLastName(e.target.value)} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign up</button>
+                </form>
+            </div>
         </div>
 
     )
