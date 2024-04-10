2e9c34f75d42457cba6e99dbd1d6005ecdb670a1
Andrew Toussaint <ajntoussaint@gmail.com>
2024-04-10 08:10:48 -0500

    moved login route back to signup.js for cleaner code

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -39,6 +39,41 @@ router.post('/signup', async (req, res) => {
     }
 });
   
+
+router.post('/login', async (req, res) => {
+    console.log("login route called")//debug
+    try {
+        // Extract form data from request body
+        const { username, password } = req.body;
   
+        // Find the user by username
+        const user = await User.findOne({ where: { username } });
+  
+        // Check if the user exists
+        if (!user) {
+            // User not found, handle accordingly (e.g., display error message)
+            return res.status(404).send('User not found');
+        }
+        
+  
+        // Compare the hashed password from the database with the password provided
+        console.log("User:", user.username, "Hashed:", user.hashedPassword);
+        console.log("Password:", password, "User password:", user.password); // Testing to see if there are values
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword); // Using hashedPassword gives 302
+  
+        if (passwordMatch) {
+            // Passwords match, redirect to dashboard or homepage
+            req.session.user = user.username;
+            res.sendStatus(200);
+        } else {
+            // Passwords don't match, handle accordingly (e.g., display error message)
+            res.status(401).send('Invalid credentials');
+        }
+    } catch (error) {
+        // Handle error (e.g., display error message)
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
