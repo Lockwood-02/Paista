d874d8de355323216ca29b8f2f6d98c5cf77979f
Andrew Toussaint <ajntoussaint@gmail.com>
2024-04-20 09:00:37 -0500

    30 passing, tested access put route

--- server/tests/accessTests.js
+++ server/tests/accessTests.js
@@ -132,6 +132,50 @@ suite('Access router tests', function(){
         })
     })
 
+    test('Get Access by bad id', function(done){
+        chai.request(server)
+        .get('/api/accesses/a')
+        .end(function(err, res){
+            assert.equal(res.status, 404)
+            assert.deepEqual(res.body, { error: 'Access not found' });
+            done();
+        })
+    })
+
+    test('Update Access type', function(done){
+        chai.request(server)
+        .put('/api/accesses/' + accessIds[0])
+        .send({
+            Users_ID: testUser.id,
+            Topic_ID: testTopics[0].id,
+            Access_Type:'moderator'
+        })
+        .end(function(err, res){
+            assert.equal(res.status,200);
+            assert.deepInclude(res.body,{
+                Users_ID: testUser.id,
+                Topic_ID: testTopics[0].id,
+                Access_Type:'moderator'
+            });
+            done();
+        })
+    })
+
+    test('Update access type with bad id', function(done){
+        chai.request(server)
+        .put('/api/accesses/a')
+        .send({
+            Users_ID: testUser.id,
+            Topic_ID: testTopics[0].id,
+            Access_Type:'moderator'
+        })
+        .end(function(err, res){
+            assert.equal(res.status,404);
+            assert.deepEqual(res.body,{ error: 'Access not found' });
+            done();
+        })
+    })
+
     suiteTeardown(async function(){
 
         await Accesses.destroy({
